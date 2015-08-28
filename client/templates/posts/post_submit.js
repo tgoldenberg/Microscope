36795bc5c440a84b175f51057db055c8df92eba3
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-28 16:26:05 -0400

    validate post contents on submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,15 @@
+Template.postSubmit.onCreated(function(){
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+})
 Template.postSubmit.events({
   'submit form': function(event) {
     event.preventDefault();
@@ -6,6 +18,11 @@ Template.postSubmit.events({
       url: $(event.target).find('[name=url]').val(),
       title: $(event.target).find('[name=title]').val()
     };
+
+    var errors = validatePost(post);
+    if (errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    }
 
     Meteor.call('postInsert', post, function(error, result) {
       if (error)
