9d2a5c5e0fd5f801704169717154cfd4e34fbf2d
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-28 13:56:32 -0400

    add edit posts form

---
+++ client/templates/posts/post_edit.js
@@ -0,0 +1,30 @@
+Template.postEdit.events({
+  'submit form': function(event) {
+    event.preventDefault();
+
+    var currentPostId = this._id;
+
+    var postProperties = {
+      url: $(event.target).find('[name=url]').val(),
+      title: $(event.target).find('[name=title]').val()
+    };
+
+    Posts.update(currentPostId, {$set: postProperties}, function(err) {
+      if (err) {
+        alert(err.reason);
+      } else {
+        Router.go('postPage', { _id: currentPostId });
+      }
+    });
+  },
+
+  'click .delete': function(event) {
+    event.preventDefault();
+
+    if (confirm('Delete this post?')) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('postsList');
+    }
+  }
+});
