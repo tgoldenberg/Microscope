6a25e422c966859f812ea887990d9d10f44f584a
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-28 13:41:23 -0400

    sort posts by submitted timestamp

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -8,12 +8,13 @@ Template.postSubmit.events({
     };
 
     Meteor.call('postInsert', post, function(error, result) {
+      console.log(result);
       if (error)
         return alert(error.reason);
 
       if (result.postExists)
         alert('This link has already been posted');
-        
+
       Router.go('postPage', { _id: result._id });
     });
   }
