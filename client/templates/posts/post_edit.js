5a628eac8b429b89b1d3b8464b2eb8e54c4ce192
tgoldenberg <thomasagoldenberg@gmail.com>
2015-08-28 20:21:47 -0400

    add active class to header navigation tabs

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -1,47 +1,95 @@
 Template.postEdit.onCreated(function() {
-  Session.set('postEditErrors');
+  Session.set('postEditErrors', {});
 });
 
 Template.postEdit.helpers({
   errorMessage: function(field) {
     return Session.get('postEditErrors')[field];
   },
-  errorClass: function(field) {
+  errorClass: function (field) {
     return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
   }
 });
 
 Template.postEdit.events({
-  'submit form': function(event) {
-    event.preventDefault();
+  'submit form': function(e) {
+    e.preventDefault();
 
     var currentPostId = this._id;
 
     var postProperties = {
-      url: $(event.target).find('[name=url]').val(),
-      title: $(event.target).find('[name=title]').val()
-    };
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
 
     var errors = validatePost(postProperties);
     if (errors.title || errors.url)
       return Session.set('postEditErrors', errors);
 
-    Posts.update(currentPostId, {$set: postProperties}, function(err) {
-      if (err) {
-        throwError(err.reason);
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
       } else {
-        Router.go('postPage', { _id: currentPostId });
+        Router.go('postPage', {_id: currentPostId});
       }
     });
   },
 
-  'click .delete': function(event) {
-    event.preventDefault();
+  'click .delete': function(e) {
+    e.preventDefault();
 
-    if (confirm('Delete this post?')) {
+    if (confirm("Delete this post?")) {
       var currentPostId = this._id;
       Posts.remove(currentPostId);
-      Router.go('postsList');
+      Router.go('home');
+    }
+  }
+});Template.postEdit.onCreated(function() {
+  Session.set('postEditErrors', {});
+});
+
+Template.postEdit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postEditErrors')[field];
+  },
+  errorClass: function (field) {
+    return !!Session.get('postEditErrors')[field] ? 'has-error' : '';
+  }
+});
+
+Template.postEdit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var currentPostId = this._id;
+
+    var postProperties = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    }
+
+    var errors = validatePost(postProperties);
+    if (errors.title || errors.url)
+      return Session.set('postEditErrors', errors);
+
+    Posts.update(currentPostId, {$set: postProperties}, function(error) {
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+      } else {
+        Router.go('postPage', {_id: currentPostId});
+      }
+    });
+  },
+
+  'click .delete': function(e) {
+    e.preventDefault();
+
+    if (confirm("Delete this post?")) {
+      var currentPostId = this._id;
+      Posts.remove(currentPostId);
+      Router.go('home');
     }
   }
 });
